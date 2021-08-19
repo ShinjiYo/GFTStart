504754eefc142e47c98bee15ad26d9d35438a7dd
ShinjiYo <61997709+ShinjiYo@users.noreply.github.com>
2021-08-19 15:15:19 -0300

    Testando

---
+++ ProjetoFinalPOO/src/projetofinalpoo/AcoesVideo.java
@@ -0,0 +1,12 @@
+
+package projetofinalpoo;
+
+/**
+ *
+ * @author Lucas Shinji Yamane
+ */
+public interface AcoesVideo {
+    public void play();
+    public void pause();
+    public void like();
+}
---
+++ ProjetoFinalPOO/src/projetofinalpoo/Gafanhoto.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetofinalpoo;
+
+/**
+ *
+ * @author Lucas Shinji Yamane
+ */
+public class Gafanhoto extends Pessoa{
+    private String login;
+    private int totAssistido;
+
+    public Gafanhoto(String nome, int idade, String sexo, String login) {
+	   super(nome, idade, sexo);
+	   this.login = login;
+	   this.totAssistido = 0;
+    }
+
+    public String getLogin() {
+	   return login;
+    }
+
+    public void setLogin(String login) {
+	   this.login = login;
+    }
+
+    public int getTotAssistido() {
+	   return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+	   this.totAssistido = totAssistido;
+    }
+
+    @Override
+    public String toString() {
+	   return "Gafanhoto{" + super.toString() + "\nlogin=" + login + ", totAssistido=" + totAssistido + '}';
+    }
+    
+    
+}
